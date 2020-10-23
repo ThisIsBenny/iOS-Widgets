d94fea37e7fd7e41c9c3f5d9c9862044034bfa76
Benny <66417262+ThisIsBenny@users.noreply.github.com>
2020-10-23 11:16:52 +0200

    Update VRR-Monitor.js

--- VRR-Monitor/VRR-Monitor.js
+++ VRR-Monitor/VRR-Monitor.js
@@ -1,6 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-green; icon-glyph: bus-alt;
+// Version 1.0.0
 
 let transportTypes = [
   {
