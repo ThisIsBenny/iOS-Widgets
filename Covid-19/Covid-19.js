47ee81e4b8e52cbaff365d7fedd7dbe946de4eaf
Benny <66417262+ThisIsBenny@users.noreply.github.com>
2020-10-23 11:19:10 +0200

    Update Covid-19.js

--- Covid-19/Covid-19.js
+++ Covid-19/Covid-19.js
@@ -1,6 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: blue; icon-glyph: microscope;
+// Version 1.0.0
 
 /**************
 Credits:
