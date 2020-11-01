37c2669e40fd3b3a373f247f01b652c3cf56e883
Benny Samir Hierl <bennysamir@posteo.de>
2020-11-01 18:17:23 +0100

    gradient added
    Signed-off-by: Benny Samir Hierl <bennysamir@posteo.de>

--- Ecosia/Ecosia.js
+++ Ecosia/Ecosia.js
@@ -15,6 +15,20 @@ if (widgetInputRAW !== null) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+let backColor; //Widget background color
+let backColor2; //Widget background color
+let textColor; //Widget text color
+
+if (Device.isUsingDarkAppearance()) {
+  backColor = '111111';
+  backColor2 = '222222';
+  textColor = 'EDEDED';
+} else {
+  backColor = 'D32D1F';
+  backColor2 = '76150C';
+  textColor = 'EDEDED';
+}
+////////////////////////////////////////////////////////////////////////////////
 async function getTreeCounter(token) {
   let req = new Request("https://api.ecosia.org/v1/accounts/personalcounter?token=" + token)
   let res = await req.loadJSON()
@@ -44,6 +58,15 @@ let treeCounter = await getTreeCounter(token);
 let ecosiaLogo = await getLogo()
 // Create Widget
 let widget = new ListWidget();
+
+const gradient = new LinearGradient()
+gradient.locations = [0, 1]
+gradient.colors = [
+  new Color(backColor),
+  new Color(backColor2)
+]
+widget.backgroundGradient = gradient
+
 widget.url = 'https://ecosia.org/'
 
 widget.setPadding(10, 10, 10, 10)
@@ -60,6 +83,7 @@ if (ecosiaLogo !== null) {
 } else {
   let title = widget.addText("Ecosia")
   title.font = Font.mediumSystemFont(12)
+  title.textColor = new Color(textColor)
   widget.addSpacer()
 }
 
@@ -68,6 +92,7 @@ counterText.font = Font.regularSystemFont(36)
 counterText.minimumScaleFactor = 0.7;
 counterText.lineLimit = 1
 counterText.centerAlignText()
+counterText.textColor = new Color(textColor)
 
 widget.addSpacer()
 
