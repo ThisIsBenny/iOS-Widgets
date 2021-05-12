4af2ea5f6126788bc7a8d0ede57cbeb478771c9d
Benny <66417262+ThisIsBenny@users.noreply.github.com>
2021-05-12 07:35:54 +0200

    Update Apple-Store-Order-Status.js
    Update version number

--- Apple-Order-Status/Apple-Store-Order-Status.js
+++ Apple-Order-Status/Apple-Store-Order-Status.js
@@ -1,7 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-blue; icon-glyph: shopping-cart;
-// Version 1.1.7
+// Version 1.2.0
 
 /// Used by enums
 const enumValue = (name) => Object.freeze({toString: () => name})
